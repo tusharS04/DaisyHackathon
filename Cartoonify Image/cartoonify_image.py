21f854a33641d359372cbb988b6e3ca97be15495
tusharS04 <109457082+tusharS04@users.noreply.github.com>
2022-08-31 13:26:25 +0530

    Update and rename Caroonify.py to cartoonify_image.py

---
+++ Cartoonify Image/cartoonify_image.py
@@ -0,0 +1,30 @@
+import cv2
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
+
+
+def cartoonify(image):
+    input_file = Image.open(sys.argv[1])
+    imgage = cv2.imread(input_file)
+    imgage = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(10,10))
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+    plt.figure(figsize=(10,10))
+    plt.imshow(gray,cmap="gray")
+
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    plt.figure(figsize=(10,10))
+    plt.imshow(edges,cmap="gray")
+
+    color = cv2.bilateralFilter(img, 9, 250, 250)
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    plt.figure(figsize=(10,10))
+    plt.imshow(cartoon,cmap="gray")
+
+    final = deepcopy(cartoon)
+
+    return final
