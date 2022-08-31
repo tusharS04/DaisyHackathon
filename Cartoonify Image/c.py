6e6d73811d3c16be84a6c374b4928399cf37aa31
tusharS04 <109457082+tusharS04@users.noreply.github.com>
2022-08-31 13:04:21 +0530

    Update c.py

--- Cartoonify Image/c.py
+++ Cartoonify Image/c.py
@@ -1,59 +1,34 @@
 import cv2
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
-#Load Image
-def read_file(filename):
-  img = cv2.imread(filename)
-  cv2_imshow(img)
-  org_img = np.copy(img)
-  return img
 
-#Create Edge Mask
-def edge_mask(img, line_size, blur_value):
-    """
-    input: Input Image
-    Output: Edges of Image
-    """
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    gray_blur = cv2.medianBlur(gray, blur_value)
+def cartoonify(image = None):
+    input_file = Image.open(sys.argv[1])
+    imgage = cv2.imread(input_file)
+    imgage = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(10,10))
 
-    edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, line_size, blur_value)
-    # return edges
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+    plt.figure(figsize=(10,10))
+    plt.imshow(gray,cmap="gray")
 
-    # line_size, blur_value = 5,7
-    edges = edge_mask(img, 5, 7)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    plt.figure(figsize=(10,10))
+    plt.imshow(edges,cmap="gray")
 
-#Reduce the Color Palette
-def color_quantizaion(img, k):
-    # Transform the image
-    data = np.float32(img).reshape((-1,3))
+    color = cv2.bilateralFilter(img, 9, 250, 250)
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    plt.figure(figsize=(10,10))
+    plt.imshow(cartoon,cmap="gray")
 
-    #Determine Criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
+    final = deepcopy(cartoon)
+    final = 1 - final
+    threshold = 0.1
+    final[final > threshold] = 1
+    final[final < threshold] = 0
 
-    # Implementing K-Means
-    ret, label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-    center = np.uint8(center)
-
-    result = center[label.flatten()]
-    result = result.reshape(img.shape)
-
-    # return result
-
-    img = color_quantizaion(img, k=9)
-
-# Combine Edge Mask with the quantiz img
-def cartoon():
-    c = cv2.bilateralFilter(img, d=3, sigmaColor=200, sigmaSpace=200)
-
-#     plt.imshow(org_img)
-#     plt.title("Original Image")
-#     plt.show()
-
-    plt.imshow(c)
-    plt.title("Cartoonified Image")
-    plt.show()
-    return c
-
-    cartoon()
+    return final
